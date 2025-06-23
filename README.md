a1c5cc2ddc39c1090c7436f987a0f0c94f69c112
sammy-ulfh <sammy_ulfh@proton.me>
2025-06-23 17:47:47 -0600

    ARP SCANNER UPDATE

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # arp_scanner
+
+<p align="center">
+    <img width="700"
+        src="images/001.png"
+        alt="Main Banner"
+        style="float: left; margin-right: 10px;">
+</p>
+
+**ARP Scanner** is a fast tool to scan active hosts and discover their Mac Address. You can use it giving the **target host** and **inerface name**.
+
+<p align="center">
+    <img width="700"
+        src="images/002.png"
+        alt="Scanning example"
+        style="float: left; margin-right: 10px;">
+</p>
+
+## What I need to run it?
+
+1. First, you need to clone the repository:
+
+    ```git
+    git clone https://github.com/sammy-ulfh/arp_scanner.git
+    ```
+
+2. Second, you need to go to **arp_scanner/script** directory.
+
+3. Third, you need to install the required libraries using pip:
+
+    ```pip3
+    pip3 install -r requirements.txt
+    ```
+
+## How can I use it?
+
+- **Target:**
+    First, you need to give a target using **-t / --target** argument.<br/>
+    Examples:<br/>
+
+    - **-t 192.168.0.1**
+    - **-t 192.168.0.0/21**
+
+- **Network Interface:**
+    Second, you need to give a specific Network Interface Name using **-i / --interface** argument.<br/>
+    Examples:
+
+    - **-i eth0**
+    - **-i ens33**
--- script/arp_scanner.py
+++ script/arp_scanner.py
@@ -13,8 +13,9 @@ def def_handler(sig, frame):
     print(colored("\n[!] Quitting the program...\n", "red"))
     sys.exit(1)
 
-signal.signal(signal.SIGINT, def_handler)
+signal.signal(signal.SIGINT, def_handler) # CTRL + C
 
+# Menu Arguments
 def get_arguments():
     argparser = argparse.ArgumentParser(description="ARP Scanner")
     argparser.add_argument("-t", "--target", required=True, dest="target", help="Host / IP Range to scan. (Ex: 192.168.100.1 / 192.168.100.0/21)")
@@ -24,44 +25,48 @@ def get_arguments():
 
     return args.target, args.interface
 
+# Verify the correct arguments format and if it's running by root.
 def verify(target, interface):
 
     if os.getuid() != 0:
         print(colored("\n[!] Root privilege required.\n", "yellow"))
         sys.exit(1)
 
-    match_uniq = re.match(r"^([0-9]{1,3}\.){3}[0-9]{1,3}$", target)
-    match_range = re.match(r"^([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}$", target)
+    match_uniq = re.match(r"^([0-9]{1,3}\.){3}[0-9]{1,3}$", target) # Verify correct format for a uniq host
+    match_range = re.match(r"^([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}$", target) # Verify correct format for range of hosts
     
     try:
-        ip, bitmask = target.split('/')
-        bitmask = int(bitmask)
-        match_range = True if bitmask < 33 else False
+        ip, bitmask = target.split('/') # get bitmask
+        bitmask = int(bitmask) # Convert to int
+        match_range = True if bitmask < 33 else False # Verify if a correct bitmask (< 33)
     except ValueError:
         pass
     
-    interfaces = [i[1] for i in socket.if_nameindex()]
-    interface = True if interface in interfaces else False
+    interfaces = [i[1] for i in socket.if_nameindex()] # Get all Network Interfaces Names on the computer.
+    interface = True if interface in interfaces else False # Verify if the Interface Name given by the user is correct.
 
-    return match_uniq or match_range, interface
+    return match_uniq or match_range, interface # return if is a correct format and if exist the interface name
 
+# Create an arp packet and starts the scan.
 def scan(ip, interface):
-    arp_packet = scapy.ARP(pdst=ip)
-    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_packet = scapy.ARP(pdst=ip) # Create arp frame
+    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") # Create Ether frame
 
-    packet = broadcast_packet/arp_packet
+    packet = broadcast_packet/arp_packet # Create the packet with ether and arp frame union.
 
     print(colored("\n[-] Sanning...\n", "blue"))
 
-    answered, unanswered = scapy.srp(packet, timeout=1, verbose=False, iface=interface)
+    answered, unanswered = scapy.srp(packet, timeout=1, verbose=False, iface=interface) # starts the scan
 
-    answers = [[ans.answer.psrc, ans.answer.src] for ans in answered]
+    answers = [[ans.answer.psrc, ans.answer.src] for ans in answered] # get only ip and mac address of each hosts
    
     return answers 
 
+# Print each hosts with green color
 def printed(li):
     print(colored(f"\t+) {li[0]} -> {li[1]}", "green"))
 
+# Print banne
 def print_banner():
     print(colored("""
 
@@ -70,6 +75,7 @@ def print_banner():
 
     print(colored("""Mᴀᴅᴇ ʙʏ sᴀᴍᴍʏ-ᴜʟғʜ\n""", 'yellow'))
 
+# Main logic
 def main():
     print_banner()
     target, interface = get_arguments()
